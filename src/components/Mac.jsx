ecf5d9792fefbc6b6aacef911de4539e93088199
Gino <castagnagino5@gmail.com>
2023-05-28 19:16:52 +0200

    mac light

--- src/components/Mac.jsx
+++ src/components/Mac.jsx
@@ -15,23 +15,35 @@ const Mac = () => {
       new TextureLoader().load('./img/websites/6.png'),
       new TextureLoader().load('./img/websites/7.png'),
       new TextureLoader().load('./img/websites/8.png'),
-      new TextureLoader().load('./img/websites/9.png')
+      new TextureLoader().load('./img/websites/9.png'),
+
     ];
   }, []);
 
   const objectsData = [
-    { position: [3, 15.5, -20], rotation: [Math.PI / -2, Math.PI, 4] },
-    { position: [3, 15.5, -25], rotation: [Math.PI / -2, Math.PI, 4] },
-    { position: [3, 15.5, -30], rotation: [Math.PI / -2, Math.PI, 4] },
-    { position: [3, 15.5, -35], rotation: [Math.PI / -2, Math.PI, 4] },
-    { position: [-3, 15.5, -20], rotation: [Math.PI / -2, Math.PI, -4] },
-    { position: [-3, 15.5, -25], rotation: [Math.PI / -2, Math.PI, -4] },
-    { position: [-3, 15.5, -30], rotation: [Math.PI / -2, Math.PI, -4] },
-    { position: [-3, 15.5, -35], rotation: [Math.PI / -2, Math.PI, -4] }
+    { position: [4.5, 15.5, -18], rotation: [Math.PI / -2, Math.PI, 4] },
+    { position: [4, 15.5, -23], rotation: [Math.PI / -2, Math.PI, 4] },
+    { position: [3.5, 15.5, -28], rotation: [Math.PI / -2, Math.PI, 4] },
+    { position: [3, 15.5, -33], rotation: [Math.PI / -2, Math.PI, 4] },
+    { position: [-3, 15.5, -18], rotation: [Math.PI / -2, Math.PI, -4] },
+    { position: [-3, 15.5, -23], rotation: [Math.PI / -2, Math.PI, -4] },
+    { position: [-3, 15.5, -28], rotation: [Math.PI / -2, Math.PI, -4] },
+    { position: [-3, 15.5, -33], rotation: [Math.PI / -2, Math.PI, -4] },
+    { position: [0, 15.5, -35], rotation: [Math.PI / -2, Math.PI, Math.PI] }
   ];
 
   return (
     <>
+      <rectAreaLight width={2.5} height={1.65} intensity={25} color={'#acb2b2'} rotation={[0, -4, 0]} position={[4.5, 15.5, -18]} />
+      <rectAreaLight width={2.5} height={1.65} intensity={25} color={'#acb2b2'} rotation={[0, -4, 0]} position={[4, 15.5, -23]} />
+      <rectAreaLight width={2.5} height={1.65} intensity={25} color={'#acb2b2'} rotation={[0, -4, 0]} position={[3.5, 15.5, -28]} />
+      <rectAreaLight width={2.5} height={1.65} intensity={25} color={'#acb2b2'} rotation={[0, -4, 0]} position={[3, 15.5, -33]} />
+      <rectAreaLight width={2.5} height={1.65} intensity={25} color={'#acb2b2'} rotation={[0, 4, 0]} position={[-3, 15.5, -18]} />
+      <rectAreaLight width={2.5} height={1.65} intensity={25} color={'#acb2b2'} rotation={[0, 4, 0]} position={[-3, 15.5, -23]} />
+      <rectAreaLight width={2.5} height={1.65} intensity={25} color={'#acb2b2'} rotation={[0, 4, 0]} position={[-3, 15.5, -28]} />
+      <rectAreaLight width={2.5} height={1.65} intensity={25} color={'#acb2b2'} rotation={[0, 4, 0]} position={[-3, 15.5, -33]} />
+      <rectAreaLight width={2.5} height={1.65} intensity={25} color={'#acb2b2'} rotation={[0, Math.PI, 0]} position={[0, 15.5, -35]} />
+
       {objectsData.map((data, index) => (
         <group key={index} position={data.position} scale={0.3} rotation={data.rotation}>
           <mesh material={materials.aluminium} geometry={nodes["Cube008"].geometry} />
